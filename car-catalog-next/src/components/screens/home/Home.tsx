dbfb5306c581a1d0201c378c86ee03456ec0cc5f
lina-gritsel <lina.gritsel@gmail.com>
2023-06-11 19:06:24 +0300

    add fonts

--- car-catalog-next/src/components/screens/home/Home.tsx
+++ car-catalog-next/src/components/screens/home/Home.tsx
@@ -1,9 +1,7 @@
 import React, { FC } from "react";
-import { Inter } from "next/font/google";
 
 import Layout from "@/components/layout/Layout";
 
-const inter = Inter({ subsets: ["latin"] });
 
 const Home: FC = () => {
   return (
